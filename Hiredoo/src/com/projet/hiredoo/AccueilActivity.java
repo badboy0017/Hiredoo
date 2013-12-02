650265f36ad58bcef72c342334842d0ada8924f3
kais <kais@kais-PC>
2013-12-02 01:45:20 +0100

    retouches !!!

---
+++ Hiredoo/src/com/projet/hiredoo/AccueilActivity.java
@@ -0,0 +1,16 @@
+package com.projet.hiredoo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccueilActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_accueil);
+	}
+	
+
+}
